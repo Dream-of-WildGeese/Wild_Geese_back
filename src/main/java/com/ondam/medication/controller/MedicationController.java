8b6a0f6d4b7fb82d6dc008c7a59eaa45a6a39094
kimjuyeon <stella2810@naver.com>
2026-08-13 21:21:48 +0900

    feat: 복용약 조회

--- src/main/java/com/ondam/medication/controller/MedicationController.java
+++ src/main/java/com/ondam/medication/controller/MedicationController.java
@@ -3,12 +3,15 @@ package com.ondam.medication.controller;
 import com.ondam.global.common.ApiResponse;
 import com.ondam.medication.dto.request.MedicationCreateRequest;
 import com.ondam.medication.dto.response.MedicationCreateResponse;
+import com.ondam.medication.dto.response.MedicationResponse;
 import com.ondam.medication.service.MedicationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "복용약", description = "복용양 등록 및 알림 설정 API")
 @RestController
@@ -27,6 +30,15 @@ public class MedicationController {
 
         return ApiResponse.success(
                 medicationService.createMedication(userId, request)
+        );
+    }
+
+    @GetMapping
+    public ApiResponse<List<MedicationResponse>> getMedications(
+            @RequestHeader("X-User-Id") Long userId
+    ) {
+        return ApiResponse.success(
+                medicationService.getMedications(userId)
         );
     }
 }
--- src/main/java/com/ondam/medication/repository/MedicationRepository.java
+++ src/main/java/com/ondam/medication/repository/MedicationRepository.java
@@ -3,6 +3,9 @@ package com.ondam.medication.repository;
 import com.ondam.medication.entity.Medication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MedicationRepository
         extends JpaRepository<Medication, Long> {
+    List<Medication> findAllByUserIdAndIsActiveTrue(Long userId);
 }
