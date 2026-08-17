3827dcfc9bda9f3c6d0e83d1d461414aaeb524d3
kimjuyeon <stella2810@naver.com>
2026-08-17 20:15:25 +0900

    feat: 로그아웃 ê¸ API

--- src/main/java/com/ondam/user/controller/UserController.java
+++ src/main/java/com/ondam/user/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.ondam.user.controller;
 
 import com.ondam.global.common.ApiResponse;
-import com.ondam.user.dto.request.HealthProfileUpdateRequest;
-import com.ondam.user.dto.request.NotificationSettingUpdateRequest;
-import com.ondam.user.dto.request.PushSubscriptionCreateRequest;
-import com.ondam.user.dto.request.UserCreateRequest;
+import com.ondam.user.dto.request.*;
 import com.ondam.user.dto.response.*;
 import com.ondam.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -91,6 +88,24 @@ public class UserController {
     ) {
 
         userService.createPushSubscription(
+                userId,
+                request
+        );
+
+        return ApiResponse.success(null);
+    }
+
+    @Operation(
+            summary = "푸시 알림 구독 해제",
+            description = "로그아웃 시 현재 브라우저의 푸시 구독 정보를 삭제합니다."
+    )
+    @DeleteMapping("/me/pushsubscriptions")
+    public ApiResponse<Void> deletePushSubscription(
+            @RequestHeader("X-User-Id") Long userId,
+            @RequestBody @Valid PushSubscriptionDeleteRequest request
+    ) {
+
+        userService.deletePushSubscription(
                 userId,
                 request
         );
